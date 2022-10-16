b0eb9f80867d342653e6f59cccadc190f44a5bbe
shawn2776 <shawn.harrington2776@gmail.com>
2022-10-16 15:43:10 -0700

    need to implement api next

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { Product } from "src/app/models/product.model";
 
 @Component({
   selector: "app-product-box",
@@ -6,7 +7,22 @@ import { Component, OnInit, Input } from "@angular/core";
 })
 export class ProductBoxComponent implements OnInit {
   @Input() fullWidthMode = false;
+  product: Product | undefined = {
+    id: 1,
+    title: "T-Shirt",
+    price: 42,
+    category: "apparel",
+    description: "T-shirt w/ logo, black, xxl",
+    image: "https://via.placeholder.com/150",
+  };
+
+  @Output() addToCart = new EventEmitter();
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onAddToCart(): void {
+    this.addToCart.emit(this.product);
+  }
 }
